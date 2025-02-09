631db6a3832f606f140f32d52ab74dca6c5859cf
williamchien <williamyulinchien@gmail.com>
2025-02-08 21:53:39 -0800

    deployment

--- src/main/java/com/Ecommence/backend/CorsConfig.java
+++ src/main/java/com/Ecommence/backend/CorsConfig.java
@@ -10,10 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200",
-                                "http://ecommencetos3.s3-website-us-east-1.amazonaws.com"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+
     }
 }
